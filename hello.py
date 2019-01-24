1145140b6e409d3e97ce0ec3d8237e96f156216d
Gabriela Gutiérrez-Colomer <46930518+gcolomer@users.noreply.github.com>
2019-01-24 09:13:32 +0100

    Add files via upload
    those are the exercises from class 1 and 2

---
+++ hello.py
@@ -0,0 +1,12 @@
+print("Hello world")
+print("We are working in net work environments")
+print("we are learning how to programme")
+print("This is exercise 1")
+
+a = 0
+while a < 20:
+    a += 1
+    print(a)
+operation = sum(range(1,101))
+print(operation)
+
