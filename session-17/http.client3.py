0e2fcb3639e34a26bf86652e84d809f23ed7715b
Gabriela Gutiérrez-Colomer <46930518+gcolomer@users.noreply.github.com>
2019-03-24 10:36:03 +0100

    Add files via upload

---
+++ session-17/http.client3.py
@@ -0,0 +1,38 @@
+import json
+import termcolor
+
+# -- Open the json file
+f = open("person4.json", 'r')
+
+# Read the data from the file
+# Now person is a dictionary with all the information
+persons = json.load(f)
+
+print("Total number of people:", len(persons["Persons"]))
+number_people = persons["Persons"]
+
+for person in number_people:
+
+    print()
+    termcolor.cprint("Name: ", 'green', end="")
+    print(person['Firstname'], person['Lastname'])
+
+    termcolor.cprint("Age: ", 'green', end="")
+    print(person['age'])
+
+    # Get the phoneNumber list
+    phoneNumbers = person['phoneNumber']
+
+    # Print the number of elements int the list
+    termcolor.cprint("Phone numbers: ", 'green', end='')
+    print(len(phoneNumbers))
+
+    # Print all the numbers
+    for i, num in enumerate(phoneNumbers):
+        termcolor.cprint("  Phone {}:".format(i), 'blue')
+
+        # The element num contains 2 fields: number and type
+        termcolor.cprint("    Type: ", 'red', end='')
+        print(num['type'])
+        termcolor.cprint("    Number: ", 'red', end='')
+        print(num['number'])
